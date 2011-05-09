82dd2cc4a5e00b3251b80962dcb6782df6eb64ab
Peter Conforto <peter_conforto@yahoo.com.au>
2011-05-09 16:49:59 +1000

    Fixed error in logic. Now it accounts for cloth and hours

--- assignment07/Task02/task02.php
+++ assignment07/Task02/task02.php
@@ -1,37 +1,47 @@
 <?php
+$clothAvailable = 160;
+$labourAvailable = 150;
 $shirtsNeeded=0;
 $shortsNeeded=0;
 $pantsNeeded =0 ;
 $bestProfit = 0;
 $machinecost=0;
 
-for ($shirt = 0; $shirt < 40; $shirt ++){
+for ($shirt = 0; $shirt < 40; $shirt ++)
+{
 	$machinecost = 0;
-	if ($shirt == 1){
-		$machinecost = $machinecost + 200;
-	}
-	
-	for ($short = 0; $short < 53; $short ++){
-		if ($short == 1){
-			$machinecost = $machinecost + 150;
+		if ($shirt == 1 )
+		{
+			$machinecost = $machinecost + 200;
+			
 		}
+	for ($short = 0; $short < 53; $short ++)
+	{
+			if ($short == 1)
+			{
+				$machinecost = $machinecost + 150;
+			}	
+		for ($pant = 0; $pant < 25; $pant ++)
+		{
+				if ($pant == 1)
+				{
+					$machinecost = $machinecost + 100;
+				}
 			
-		for ($pant = 0; $pant < 25; $pant ++){
-			if ($pant == 1){
-				$machinecost = $machinecost + 100;
-			}
-			
-			$profit = ($shirt * 6)+($short * 4)+($pant * 7);
-			$totalprofit = ($profit - $machinecost);
-			if ($bestProfit < ($totalprofit)){
-					$bestProfit = $totalprofit;
-					$shirtsNeeded = $shirt;
-					$shortsNeeded = $short;
-					$pantsNeeded = $pant;
-					
-			}
+				$profit = ($shirt * 6)+($short * 4)+($pant * 7);
+				$labourUsed = ($shirt * 3)+($short * 2)+($pant * 6);
+				$clothUsed = ($shirt * 4)+($short * 3)+($pant * 4);
 				
-		}
+				$totalprofit = ($profit - $machinecost);
+				if (($bestProfit < ($totalprofit)) && ($labourUsed <= $labourAvailable) && ($clothUsed <= $clothAvailable))
+				{
+						$bestProfit = $totalprofit;
+						$shirtsNeeded = $shirt;
+						$shortsNeeded = $short;
+						$pantsNeeded = $pant;
+						
+				}
+		}	
 	}
 }
 	
